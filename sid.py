3a4098bd04d73353d341e157fc4b1243e7db7d85
András Veres-Szentkirályi <vsza@vsza.hu>
2010-02-23 21:16:37 +0100

    Added waveform and voice control to Python class

--- sid.py
+++ sid.py
@@ -5,17 +5,23 @@ from subprocess import Popen, PIPE
 import time
 
 class SID:
+	NOISE = 0x80
+	SQUARE = 0x40
+	RAMP = 0x20
+	TRIANGLE = 0x10
 	def __init__(self):
 		self.process = Popen(['./cat'], stdin=PIPE)
+		self.voice = 0
+		self.waveform = SID.RAMP
 
 	def rawrite(self, addr, data):
 		self.process.stdin.write(chr(addr) + chr(data))
 
 	def playfreq(self, freq, delay):
-		self.rawrite(0x00, freq & 0xFF)
-		self.rawrite(0x01, (freq >> 8) & 0xFF)
-		self.rawrite(0x04, 0x00)
-		self.rawrite(0x04, 0x21)
+		self.rawrite(self.voice * 7, freq & 0xFF)
+		self.rawrite(self.voice * 7 + 1, (freq >> 8) & 0xFF)
+		self.rawrite(self.voice * 7 + 4, 0x00)
+		self.rawrite(self.voice * 7 + 4, self.waveform | 0x01)
 		time.sleep(delay)
 
 	def playmidinote(self, note, delay):
